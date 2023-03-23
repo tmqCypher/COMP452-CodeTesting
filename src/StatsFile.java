e8d1a3244514ab4a9a9949ce6248fafa4c9bd3a0
Quinn Cypher <tmqCypher.git@gmail.com>
2023-03-23 18:14:51 -0400

    Minor refactor of StatsFile

--- src/StatsFile.java
+++ src/StatsFile.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -16,7 +17,6 @@ import java.util.TreeMap;
 public class StatsFile extends GameStats {
     public static final String FILENAME = "guess-the-number-stats.csv";
 
-
     // maps the number of guesses required to the number of games within
     // the past 30 days where the person took that many guesses
     private SortedMap<Integer, Integer> statsMap;
@@ -24,18 +24,21 @@ public class StatsFile extends GameStats {
     public StatsFile(){
         statsMap = new TreeMap<>();
         LocalDateTime limit = LocalDateTime.now().minusDays(30);
+        for (int numGuesses : loadStatsAfter(limit)) {
+            statsMap.put(numGuesses, 1 + statsMap.getOrDefault(numGuesses, 0));
+        }
+    }
 
+    private ArrayList<Integer> loadStatsAfter(LocalDateTime limit) {
+        ArrayList<Integer> results = new ArrayList<>();
         try (CSVReader csvReader = new CSVReader(new FileReader(FILENAME))) {
-            String[] values = null;
+            String[] values;
             while ((values = csvReader.readNext()) != null) {
                 // values should have the date and the number of guesses as the two fields
                 try {
                     LocalDateTime timestamp = LocalDateTime.parse(values[0]);
                     int numGuesses = Integer.parseInt(values[1]);
-
-                    if (timestamp.isAfter(limit)) {
-                        statsMap.put(numGuesses, 1 + statsMap.getOrDefault(numGuesses, 0));
-                    }
+                    if (timestamp.isAfter(limit)) results.add(numGuesses);
                 }
                 catch(NumberFormatException nfe){
                     // NOTE: In a full implementation, we would log this error and possibly alert the user
@@ -53,6 +56,7 @@ public class StatsFile extends GameStats {
             // NOTE: In a full implementation, we would log this error and alert the user
             // NOTE: For this project, you do not need unit tests for handling this exception.
         }
+        return results;
     }
 
     @Override
